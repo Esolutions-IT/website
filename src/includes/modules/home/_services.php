fda7cdc41b828374d59621801492221f8936515f
Gertjan <info@esolutions-it.nl>
2022-01-13 19:49:30 +0100

    Services
    Services

--- src/includes/modules/home/_services.php
+++ src/includes/modules/home/_services.php
@@ -1,3 +1,67 @@
 <div class="services__wrapper">
+    <div class="services__container">
+        <div class="services__content">
+            <div class="services__head">
+                <h1 class="services__head--title">Diensten</h1>
+            </div>
+            <div class="services">
+                <div class="services__item" style="padding-right:58px;">
+                    <div class="item">
+                        <div class="item__header">
+                            <div class="item__header--grayCircle">
+                                <div class="item__header--whiteCircle">
+                                    <img class="item__header--icon" src="public/gfx/icons/internet.png">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item__content">
+                            <h1 class="item__content--title">Hosting</h1>
+                            <p class="item__content--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis dui non magna sollicitudin maximus.</p>
+                        </div>
+                        <div class="item__footer">
+                            <h1 class="item__footer--text">Meer informatie</h1>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="services__item" style="padding-right:58px;">
+                    <div class="item">
+                        <div class="item__header">
+                            <div class="item__header--grayCircle">
+                                <div class="item__header--whiteCircle">
+                                    <img class="item__header--icon" src="public/gfx/icons/website.png">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item__content">
+                            <h1 class="item__content--title">Website</h1>
+                            <p class="item__content--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis dui non magna sollicitudin maximus.</p>
+                        </div>
+                        <div class="item__footer">
+                            <h1 class="item__footer--text">Meer informatie</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="services__item">
+                    <div class="item">
+                        <div class="item__header">
+                            <div class="item__header--grayCircle">
+                                <div class="item__header--whiteCircle">
+                                    <img class="item__header--icon" src="public/gfx/icons/webshop.png">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item__content">
+                            <h1 class="item__content--title">Webshop</h1>
+                            <p class="item__content--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis dui non magna sollicitudin maximus.</p>
+                        </div>
+                        <div class="item__footer">
+                            <h1 class="item__footer--text">Meer informatie</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
