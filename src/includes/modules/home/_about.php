984e0f5e38754dd8899544b17d9b5cad45ff608c
Gertjan <info@esolutions-it.nl>
2022-01-14 11:49:22 +0100

    Text update
    text update

--- src/includes/modules/home/_about.php
+++ src/includes/modules/home/_about.php
@@ -3,9 +3,9 @@
     <div class="about__collumn">
         <div class="about__text" points="0 250, 50 300, 300 300, 300 50, 250 0, 0 0">
             <h1 class="about__text--title">Over Esolutions-IT</h1>
-            <p class="about__text--paragraphs">Esolutions-IT is opgericht in 2018 door Gertjan Egas. Bij Esolutions-IT kunt u terecht als u een website of een webapplicatie nodig hebt. Maar ook kunt u een hosting pakket bij ons af sluiten.</p>
+            <p class="about__text--paragraphs">Esolutions-IT is opgericht in 2018 door Gertjan Egas. Bij Esolutions-IT kunt u terecht als u een website of een webapplicatie nodig hebt. Ook kunt u een hosting pakket bij ons afsluiten.</p>
             <br/>
-            <p class="about__text--paragraphs">De naam Esolutions-IT is ontstaan door de achternaam van Gertjan genaamd ‘Egas’. Vervolgens staat er solutions-it omdat Esolutions-IT, IT diensten aanbiedt.</p>
+            <p class="about__text--paragraphs">De naam Esolutions-IT is ontstaan door de eerste letter van de achternaam van Gertjan: ‘Egas’. Vervolgens staat er solutions-IT omdat Esolutions-IT, IT diensten aanbiedt.</p>
         </div>
     </div>
     <div class="about__collumn">
